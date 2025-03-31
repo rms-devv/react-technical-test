8c16fd39587fd9aafbeee524816ada15364f3863
rms-devv <rms.mdev@gmail.com>
2025-03-31 11:14:09 +0200

    add filter function in user store

--- src/Store/userStore.ts
+++ src/Store/userStore.ts
@@ -1,3 +1,4 @@
+// src/store/userStore.ts
 import { create } from 'zustand';
 
 interface User {
@@ -8,13 +9,19 @@ interface User {
 
 interface UsersState {
   users: Record<string, User>;
+  hiddenUsers: Set<string>; // Utilisateurs dont les messages sont masqués
   addUser: (login: string, avatar_url: string) => void;
   incrementMessageCount: (login: string) => void;
   resetUsers: () => void;
+  toggleUserVisibility: (login: string) => void;
+  showAllUsers: () => void;
+  hideAllUsers: () => void;
+  isUserHidden: (login: string) => boolean;
 }
 
-export const useUsersStore = create<UsersState>((set) => ({
+export const useUsersStore = create<UsersState>((set, get) => ({
   users: {},
+  hiddenUsers: new Set<string>(),
 
  // Permet d'ajouter un utilisateur si il n'existe pas déjà
  // J'évite les doublons en vérifiant si l'user existe déjà
@@ -51,5 +58,28 @@ export const useUsersStore = create<UsersState>((set) => ({
     };
   }),
   // Fonction qui me permet de réinitialiser la liste des utilisateurs, lorsque l'issue change
-  resetUsers: () => set({ users: {} })
+  resetUsers: () => set({ users: {}, hiddenUsers: new Set<string>() }),
+
+  // Gère la visibilité d'un utilisateur
+  toggleUserVisibility: (login) => set((state) => {
+    const newHiddenUsers = new Set(state.hiddenUsers);
+    if (newHiddenUsers.has(login)) {
+      newHiddenUsers.delete(login);
+    } else {
+      newHiddenUsers.add(login);
+    }
+    return { hiddenUsers: newHiddenUsers };
+  }),
+
+  // Affiche tous les utilisateurs
+  showAllUsers: () => set({ hiddenUsers: new Set<string>() }),
+
+  // Cacher tous les utilisateurs
+  hideAllUsers: () => set((state) => {
+    const allUsers = new Set(Object.keys(state.users));
+    return { hiddenUsers: allUsers };
+  }),
+
+  // Fonction qui me permet si un utilisateur est caché
+  isUserHidden: (login) => get().hiddenUsers.has(login),
 }));
